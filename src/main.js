13dcde0bfc4e57c178404c59e2b626183a9b91bc
Juejuesun <48380934+Juejuesun@users.noreply.github.com>
2020-04-29 23:04:44 +0800

    Revert "修复bug 添加头像"
    This reverts commit 484957c4745613135e9b7c3a3c0a3032c49ba6a9.

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import VueSocketIo from 'vue-socket.io'
 import SocketIO from "socket.io-client"
 import axios from 'axios'
 //配置请求根路径
-axios.defaults.baseURL = 'http://localhost:8080/mock'//测试用
-// axios.defaults.baseURL = 'http://127.0.0.1/'//正式用
+// axios.defaults.baseURL = 'http://localhost:8080/mock'//测试用
+axios.defaults.baseURL = 'http://127.0.0.1/'//正式用
 // axios.interceptors.request.use(config => {
 //   console.log(config)
 //   config.headers.Authorization = window.sessionStorage.getItem('token')
@@ -20,8 +20,8 @@ Vue.prototype.$http = axios
 
 Vue.use(new VueSocketIo({
   debug: true,
-  connection: SocketIO('http://localhost:8081'),//测试接口
-  // connection: SocketIO('http://127.0.0.1/chat'),
+  // connection: SocketIO('http://localhost:8081'),//测试接口
+  connection: SocketIO('http://127.0.0.1/chat'),
   vuex: {
     store,
     mutationPrefix: "SOCKET_",
